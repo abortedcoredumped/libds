6a188da309f4ffd91e8e0adcb49b0c362771a21e
Lucian A. Marin <sendrecv@outlook.com>
2026-09-03 23:15:55 +0800

    add some comments

--- include/stack.h
+++ include/stack.h
@@ -5,14 +5,19 @@
 
 typedef Array Stack;
 
+// Make a stack
 Stack *make_stack(size_t capacity);
 
+// Free a stack
 int free_stack(Stack *target);
 
+// Push a data at the tail
 int stack_push(Stack *target, void *value);
 
+// Pop out the top
 void *stack_pop(Stack *target);
 
+// Get the top
 void *stack_top(Stack *target);
 
 #endif
