56e2844bf6948e63cd7cffe0237edc001d9d073f
Lucian A. Marin <sendrecv@outlook.com>
2026-09-03 17:12:28 +0800

    implement set, push, get functions

--- array.c
+++ array.c
@@ -35,3 +35,56 @@ int free_array(struct array *target)
 
     target = NULL;
 }
+
+int array_set(struct array *target, size_t index, void *value)
+{
+    if (target == NULL) {
+        return ARRAY_NULL_POINTER;  
+    }
+
+    if (target->address == NULL) {
+        return ARRAY_NULL_BASE_ADR;
+    }
+
+    if (index < 0 || index > target->length) {
+        return ARRAY_ILLEGAL_INDEX;
+    }
+
+    if (target->length == target->capacity) {
+        void **tmp = 
+            realloc(
+                target->address,
+                (ARRAY_CAP_PLUS+target->capacity) * sizeof(void*)
+            );
+        
+        if (tmp == NULL) {
+            return ARRAY_FAIL_ALLOC_MEM;
+        }
+
+        target->address = tmp;
+        target->capacity += ARRAY_CAP_PLUS;
+    }
+
+    target->address[index] = value;
+    
+    if (index == target->length) {
+        target->length++;
+    } 
+
+    return 0;
+}
+
+void *array_get(struct array *target, size_t index)
+{
+    if (target == NULL || target->address == NULL ||
+        index < 0 || index >= target->capacity) {
+        return NULL;
+    }
+
+    return target->address[index];
+}
+
+int array_push(struct array *target, void *value)
+{
+    return array_set(target, target->length, value);
+}
