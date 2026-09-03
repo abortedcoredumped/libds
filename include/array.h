7b8a2dec8b86432ec4ebe6dd6d95d4164f7947a6
Lucian A. Marin <sendrecv@outlook.com>
2026-09-03 17:12:16 +0800

    declear set, push, get functions

--- include/array.h
+++ include/array.h
@@ -10,9 +10,17 @@ struct array {
     size_t capacity;
 };
 
+// Exceptions
 enum {
-    ARRAY_NULL_POINTER,
-    ARRAY_NULL_BASE_ADR
+    ARRAY_NULL_POINTER = 1,
+    ARRAY_NULL_BASE_ADR,
+    ARRAY_ILLEGAL_INDEX,
+    ARRAY_FAIL_ALLOC_MEM
+};
+
+// Resize increments
+enum {
+    ARRAY_CAP_PLUS = 10
 };
 
 // Make and return an array, you should provide
@@ -24,9 +32,14 @@ struct array *make_array(size_t capacity);
 int free_array(struct array *target);
 
 // Set a value to a given index of an array
+// Returns 0 when success
 int array_set(struct array *target, size_t index, void *value);
 
 // Push a value at the tail of an array
+// Return value see array_set()
 int array_push(struct array *target, void *value);
+
+// Get a value from a specific index
+void *array_get(struct array *target, size_t index);
 
 #endif
