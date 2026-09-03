06d24b4137876c37422be76a847e045f4f413012
Lucian A. Marin <sendrecv@outlook.com>
2026-09-03 19:53:46 +0800

    add some necessary comments

--- include/list.h
+++ include/list.h
@@ -22,14 +22,19 @@ enum {
     LIST_FAIL_ALLOC_MEM
 };
 
+// This function returns a linked-list node
 struct node *make_node(void *value, struct node *prev, struct node *next);
 
+// Initialize a empty list
 struct list *make_list(void);
 
+// Free a list from its head to its tail
 int free_list(struct list *target);
 
+// Push a value to a list at the end of the list
 int list_push(struct list *target, void *value);
 
+// Push a value the the from the front
 int list_unshift(struct list *target, void *value);
 
 #endif
