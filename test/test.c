a06a718f51b0effbdcf23968629884fedbee89f0
Lucian A. Marin <sendrecv@outlook.com>
2026-09-04 16:10:06 +0800

    test

--- test/test.c
+++ test/test.c
@@ -1,14 +1,22 @@
 #include <array.h>
+#include <common/reverse.h>
 
 #include <stdio.h>
 
 int main(void)
 {
     Array *arr = make_array(2);
+    int iarr[] = {2, 4, 6, 8};
     
-    for (size_t i = 0; i < 10000; i++) {
-        array_push(arr, &i);
-        printf("val=%zu, len=%zu, cap=%zu\n", *(size_t*)arr->address[i],arr->length,arr->capacity);
+    array_push(arr, &iarr[0]);
+    array_push(arr, &iarr[1]);
+    array_push(arr, &iarr[2]);
+    array_push(arr, &iarr[3]);
+
+    reverse(arr->address, sizeof(void *), 4);
+
+    for (int i = 0; i < 4; i++) {
+        printf("%d\n", *(int *)arr->address[i]);
     }
 
     free_array(arr);
